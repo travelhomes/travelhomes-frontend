816cea2f686f7044599f3d112e5866743fc04a7e
Vairag shah <vairagshah55@gmail.com>
2025-04-05 09:56:29 +0530

    config file changes

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,17 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.pexels.com', 'images.unsplash.com', 'api.dicebear.com', 'maps.googleapis.com', 'maps.gstatic.com', 'pbs.twimg.com'],
+    domains: ['3.7.52.212:5000', 'localhost', 'images.pexels.com', 'images.unsplash.com', 'api.dicebear.com', 'maps.googleapis.com', 'maps.gstatic.com', 'pbs.twimg.com'],
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: 'http://3.7.52.212:5000/:path*', // Proxy to HTTP server
+      },
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/:path*', // Proxy to HTTP server
       },
     ];
   },
